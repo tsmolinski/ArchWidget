5556795eaa6d2657723e534b4f92918aa14b0960
tsmolinski <tsmolinski3@gmail.com>
2024-11-27 11:46:22 +0100

    Added Base UI widget.

---
+++ Source/ArchWidget/UI/ArchBaseUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "ArchBaseUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHWIDGET_API UArchBaseUI : public UCommonUserWidget
+{
+	GENERATED_BODY()
+	
+};
